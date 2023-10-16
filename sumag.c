e615e87b4f9d57b5cd6bdbb3ab7b7c84f0120bcc
Laboratorio <laboratorio@udla-ec.int>
2023-10-16 16:44:06 -0500

    Aparicion del valor

--- sumag.c
+++ sumag.c
@@ -3,6 +3,7 @@ int main (){
 int num,sumagau;
 printf("Ingresa Numero\n");
 scanf("%d", &num);
-sumagau=(num*(num/1));
+sumagau=(num*(num/1))/2;
+printf("La suma de Gauus es de: %d",sumagau);
     return 0;
 }
